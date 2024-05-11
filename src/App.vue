c9edc1f24d8ed5d608668f5b2e33e3859a98070f
Jakub <jjtusan@gmail.com>
2024-05-11 10:38:06 +0200

    Cleanup of default Vue welcome components

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import PlaygroundPage from './components/PlaygroundPage.vue';
 </script>
 
 <template>
-  <PlaygroundPage />
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <PlaygroundPage />
   </main>
 </template>
-
-<style scoped>
-section {
-  width: 100%;
-  height: 85vh;
-  border: 2px dashed #AAA;
-  margin: 15px auto;
-}
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
